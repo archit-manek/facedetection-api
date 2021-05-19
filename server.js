6717bd48c448ca49c81f0110ddb3603998d7d7a3
Archit <architm@Archits-MacBook-Air-2.local>
2021-05-19 00:44:02 -0400

    database connection

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const { response } = require("express");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-shallow-50863",
-    user: "",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl = true,
   },
 });
 
