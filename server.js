48748ab6dcf04e04b83221be406e082a197ff37c
Archit <architm@Archits-MacBook-Air-2.local>
2021-05-19 20:46:29 -0400

    db

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const bcrypt = require("bcrypt");
 const cors = require("cors");
 const knex = require("knex");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
