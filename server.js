96ec7ff6761e57405ea19f016f8a9c58b1545e37
Archit <architm@Archits-MacBook-Air-2.local>
2021-05-19 20:43:25 -0400

    db

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post("/register", (req, res) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("error registering"));
+  }).catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 app.get("/profile/:id", (req, res) => {
